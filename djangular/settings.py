c6e8c990f65763f9e09d1b5ecea96ea447c24a97
Anthony Zabonik <anthony.zabonik@gmail.com>
2017-06-06 22:20:56 -0400

    updated PROJECT_DIR

--- djangular/settings.py
+++ djangular/settings.py
@@ -127,5 +127,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_DIR = os.path.join(os.path.dirname(__file__), '..')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
